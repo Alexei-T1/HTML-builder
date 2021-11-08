51ad802c35e9f770b11151b865c5f900e9e0f2b9
Alexei-T1 <turencko.alexei@yandex.ru>
2021-11-08 18:35:16 +0400

    feat: 1st task

---
+++ 01-read-file/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+try {
+  const pathCurrentText = path.basename('./text.txt');
+  const pathCurrentDir =path.dirname('./index');
+
+  const pathStrText = path.resolve(__dirname, pathCurrentDir, pathCurrentText);
+
+  const stream = fs.createReadStream(pathStrText, 'utf-8');
+  stream.on('data', writeToConsole);
+  
+} catch (error) {
+  console.log('неверный путь', error.message);
+}
+  
+
+
+function writeToConsole(partOfText) {
+  console.log('\n', partOfText);
+}
